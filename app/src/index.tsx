ea10aa6c34e61feea97b1d7757fe28314dc7eac9
jamal.santos <giovanni.santos@hotvia.com.br>
2020-08-21 00:43:54 -0300

    style: css

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import logoImg from '../../assets/images/logo.svg';
-import landingImg from '../../assets/images/landing.svg';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-
-function Landing() {
-  return (
-    <div id="page-landing">
-      <div id="page-landing-content" className="container">
-        <div className="logo-container">
-          <img src={logoImg} alt="Proffy" />
-          <h2>Sua Plataforma de estudos online.</h2>
-        </div>
-        <img src={landingImg} alt="Plataforma de estudos" className="hero-image" />
-        <div className="buttons-container">
-          <a href="" className="study"></a>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default Landing;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
